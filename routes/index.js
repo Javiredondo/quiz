142df9c8439690eac9f6f54dd6ff639b53e0ea26
Javiredondo <Javiredondo@users.noreplay.github.com>
2015-07-30 12:57:15 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.param('quizId', quizController.load);  // autoload: quizID
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 //Definición de ruta author
 router.get('/author',                      quizController.author)
